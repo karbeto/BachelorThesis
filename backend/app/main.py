fc37bbc37953bbd29123a141f7febad23d9fc1ed
karbeto <karbevskk@gmail.com>
2026-05-17 12:44:53 +0200

    Mobile app setup complete

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth.router)
 app.include_router(users.router)
